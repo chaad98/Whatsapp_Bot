da3cf987ae57abdc34eba577ae5bb97d52416727
chaad98 <faritzchad@yahoo.com>
2023-11-14 19:44:26 +0800

    Update connection with multi devices

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const { DisconnectReason, useMultiFileAuthState } = require("@whiskeysockets/bai
 
 const makeWASocket = require('@whiskeysockets/baileys').default;
 
-async function connectionLogic() {
+const {
+    Browsers,
+  } = require("@whiskeysockets/baileys");
+
+async function connectToWhatsApp() {
     const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys')
 
     const sock = makeWASocket({
         // can provide additional config here
         printQRInTerminal: true,
         auth: state,
+        browser: Browsers.ubuntu("Testing"),
     });
 
     sock.ev.on("connection.update", async (update) => {
@@ -22,11 +27,27 @@ async function connectionLogic() {
         // connection is close
         if (connection == "close") {
             const shouldReconnect = lastDisconnect?.error?.output?.statusCode != DisconnectReason.loggedOut;
+            console.log(`Connection closed due to ${lastDisconnect?.error}, reconnecting: ${shouldReconnect}`);
 
             // connections is reconnected
             if (shouldReconnect) {
-                connectionLogic();
+                connectToWhatsApp();
             }
+
+        } else if (connection === "open") {
+            console.log("Connection opened");
+
+            sock.ev.on("messages.upsert", (messageInfoUpsert) => {
+                console.log(JSON.stringify(messageInfoUpsert, undefined, 2));
+            
+                // Example: reply to the first message from someone else with "Hello there!"
+                const remoteJid = messageInfoUpsert.messages[0]?.key?.remoteJid;
+                const fromMe = messageInfoUpsert.messages[0]?.key?.fromMe;
+            
+                if (remoteJid && !fromMe) {
+                    sock.sendMessage(remoteJid, { text: 'This message from server WhatsApp Business' });
+                }
+            });
         }
     });
 
@@ -34,11 +55,7 @@ async function connectionLogic() {
         console.log(messageInfo);
     });
 
-    sock.ev.on("messages.upsert", (messageInfoUpsert) => {
-        console.log(messageInfoUpsert);
-    });
-
     sock.ev.on ("creds.update", saveCreds)
 }
 
-connectionLogic();
+connectToWhatsApp();
