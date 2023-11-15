b7868bd1b603965aff3dd62b67653b4166bbbc5b
chaad98 <faritzchad@yahoo.com>
2023-11-15 17:58:34 +0800

    Update v3

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { DisconnectReason, useMultiFileAuthState } = require('@whiskeysockets/bai
 const makeWASocket = require('@whiskeysockets/baileys').default;
 const { Browsers } = require('@whiskeysockets/baileys');
 const qrCode = require('qrcode'); // Import qrcode library
+const axios = require('axios');
 
 
 //----- Setting up Express -----//
@@ -18,35 +19,67 @@ let sock;
 
 
 //----- API Routes -----//
-app.get('/getQRCode', async (req, res) => {
-    try {
-        // Generate QR code image
-        const pngBuffer = await qrCode.toBuffer(qrCodeData, { type: 'png' });
+// app.post('/sendQRCode', async (req, res) => {
+//     try {
+//         // Generate QR code image
+//         const pngBuffer = await qrCode.toBuffer(qrCodeData, { type: 'png' });
 
-        // Convert the image buffer to base64
-        const base64QRCodeImage = pngBuffer.toString('base64');
+//         // Convert the image buffer to base64
+//         const base64QRCodeImage = pngBuffer.toString('base64');
 
-        // Construct HTML with an embedded image
-        const htmlResponse = `<html><body>QR Code Below <br><img src="data:image/png;base64,${base64QRCodeImage}" alt="QR Code"></body></html>`;
+//         // Make an HTTP POST request to the API with the base64QRCodeImage in the request body
+//         const apiEndpoint = 'https://www.api-controller.com/api/whatsapp_qr_api.php';
+//         const apiResponse = await axios.post(apiEndpoint, {
+//             base64QRCodeImage: base64QRCodeImage
+//         });
 
-        // Send the HTML response
-        res.send(htmlResponse);
-    } catch (error) {
-        console.error('Error generating QR code:', error);
-        res.status(500).json({ success: false, error: 'Internal Server Error' });
-    }
-});
+//         // Log the response from the API
+//         console.log('API Response:', apiResponse.data);
 
-app.post('/sendMessage', (req, res) => {
-    const { remoteJid, message } = req.body;
+//         // Send the HTML response
+//         res.send(base64QRCodeImage);
+//         console.log(`Base 64 String image converted: ${base64QRCodeImage}`);
+//     } catch (error) {
+//         console.error('Error generating QR code or making API request:', error);
+//         res.status(500).json({ success: false, error: 'Internal Server Error' });
+//     }
+// })
 
-    if (remoteJid && message && sock) {
-        sock.sendMessage(remoteJid, { text: message });
-        res.json({ success: true });
-    } else {
-        res.status(400).json({ success: false, error: 'Invalid request body or sock is not initialized' });
-    }
-});
+// app.get('/getMessage', (req, res) => {
+//     const remoteJid = req.query.remoteJid;
+//     const message = req.query.message;
+
+//     if (remoteJid && message && sock) {
+//         sock.sendMessage(remoteJid, { text: message });
+
+//         // Make an HTTP POST request to the API with the message details
+//         const apiEndpoint = 'https://www.api-controller.com/api/whatsapp_qr_api.php';
+//         axios.post(apiEndpoint, {
+//             fromJid: remoteJid,  // Only send the fromJid data to the API
+//             message: message
+//         }).then(apiResponse => {
+//             // Log the response from the API
+//             console.log('API Response:', apiResponse.data);
+//         }).catch(error => {
+//             console.error('Error making API request:', error);
+//         });
+
+//         res.json({ success: true });
+//     } else {
+//         res.status(400).json({ success: false, error: 'Invalid request parameters or sock is not initialized' });
+//     }
+// });
+
+// app.post('/sendMessage', (req, res) => {
+//     const { remoteJid, message } = req.body;
+
+//     if (remoteJid && message && sock) {
+//         sock.sendMessage(remoteJid, { text: message });
+//         res.json({ success: true });
+//     } else {
+//         res.status(400).json({ success: false, error: 'Invalid request body or sock is not initialized' });
+//     }
+// });
 
 
 //----- WhatsApp API Setup -----//
@@ -62,13 +95,34 @@ async function connectToWhatsApp() {
     sock.ev.on('connection.update', async (update) => {
         const { connection, lastDisconnect, qr } = update || {};
 
-        // update qr code
-        if (qr) {
-            qrCodeData = qr;
+        // Delay for 8 seconds
+        setTimeout(async () => {
+            // update qr code
+            if (qr) {
+                qrCodeData = qr;
 
-            // Convert the QR code data to base64 and perform any additional actions here
-            console.log("QR Code Data: " + qrCodeData);
-        }
+                // Convert the QR code data to base64
+                const pngBuffer = await qrCode.toBuffer(qrCodeData, { type: 'png' });
+                const base64QRCodeImage = pngBuffer.toString('base64');
+
+                try {
+                    // Make an HTTP POST request to the API with the base64QRCodeImage in the request body
+                    const apiEndpoint = 'https://www.api-controller.com/api/whatsapp_qr_api.php';
+                    const apiResponse = await axios.post(apiEndpoint, {
+                        base64QRCodeImage: base64QRCodeImage
+                    });
+
+                    // Log the response from the API
+                    console.log('API Response:', apiResponse.data);
+                    console.log(base64QRCodeImage);
+                } catch (error) {
+                    console.error('Error making API request:', error);
+                }
+
+            // // Additional actions if needed
+            // console.log("QR Code Data: " + qrCodeData);
+            }
+        });
 
         if (connection == 'close') {
             const shouldReconnect = lastDisconnect?.error?.output?.statusCode != DisconnectReason.loggedOut;
@@ -77,7 +131,8 @@ async function connectToWhatsApp() {
             if (shouldReconnect) {
                 connectToWhatsApp();
             }
-        } else if (connection === 'open') {
+        } 
+        else if (connection === 'open') {
             console.log('Connection opened');
 
             sock.ev.on('messages.upsert', (messageInfoUpsert) => {
@@ -87,12 +142,32 @@ async function connectToWhatsApp() {
                 const fromMe = messageInfoUpsert.messages[0]?.key?.fromMe;
 
                 if (remoteJid && !fromMe) {
-                    console.log('Sending 10 seconds delay text messages');
+                    const messageText = messageInfoUpsert.messages[0]?.message?.conversation;
+        
+                    if (messageText) {
+                        console.log(`Received message: ${messageText}`);
+        
+                        // Make an HTTP POST request to the API with the message text
+                        const apiEndpoint = 'https://www.api-controller.com/api/whatsapp_qr_api.php';
+                        axios.post(apiEndpoint, {
+                            fromJid: remoteJid,
+                            message: messageText
+                        }).then(apiResponse => {
+                            // Log the response from the API
+                            console.log('API Response:', apiResponse.data);
+                        }).catch(error => {
+                            console.error('Error making API request:', error);
+                        });
+                    }
 
-                    setTimeout(() => {
-                        sock.sendMessage(remoteJid, { text: 'This message from server WhatsApp Business' });
-                        console.log('Delayed reply sent');
-                    }, 10000);
+                    // if (remoteJid && !fromMe) {
+                    //     console.log('Sending 10 seconds delay text messages');
+    
+                    //     setTimeout(() => {
+                    //         sock.sendMessage(remoteJid, { text: 'This message from server WhatsApp Business' });
+                    //         console.log('Delayed reply sent');
+                    //     }, 10000);
+                    // }
                 }
             });
         }
