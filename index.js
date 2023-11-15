fd13074aac0789675dcc28f44f71236f27f4f542
chaad98 <faritzchad@yahoo.com>
2023-11-15 23:04:03 +0800

    Update

--- index.js
+++ index.js
@@ -18,70 +18,6 @@ let qrCodeData = '';
 let sock;
 
 
-//----- API Routes -----//
-// app.post('/sendQRCode', async (req, res) => {
-//     try {
-//         // Generate QR code image
-//         const pngBuffer = await qrCode.toBuffer(qrCodeData, { type: 'png' });
-
-//         // Convert the image buffer to base64
-//         const base64QRCodeImage = pngBuffer.toString('base64');
-
-//         // Make an HTTP POST request to the API with the base64QRCodeImage in the request body
-//         const apiEndpoint = 'https://www.api-controller.com/api/whatsapp_qr_api.php';
-//         const apiResponse = await axios.post(apiEndpoint, {
-//             base64QRCodeImage: base64QRCodeImage
-//         });
-
-//         // Log the response from the API
-//         console.log('API Response:', apiResponse.data);
-
-//         // Send the HTML response
-//         res.send(base64QRCodeImage);
-//         console.log(`Base 64 String image converted: ${base64QRCodeImage}`);
-//     } catch (error) {
-//         console.error('Error generating QR code or making API request:', error);
-//         res.status(500).json({ success: false, error: 'Internal Server Error' });
-//     }
-// })
-
-// app.get('/getMessage', (req, res) => {
-//     const remoteJid = req.query.remoteJid;
-//     const message = req.query.message;
-
-//     if (remoteJid && message && sock) {
-//         sock.sendMessage(remoteJid, { text: message });
-
-//         // Make an HTTP POST request to the API with the message details
-//         const apiEndpoint = 'https://www.api-controller.com/api/whatsapp_qr_api.php';
-//         axios.post(apiEndpoint, {
-//             fromJid: remoteJid,  // Only send the fromJid data to the API
-//             message: message
-//         }).then(apiResponse => {
-//             // Log the response from the API
-//             console.log('API Response:', apiResponse.data);
-//         }).catch(error => {
-//             console.error('Error making API request:', error);
-//         });
-
-//         res.json({ success: true });
-//     } else {
-//         res.status(400).json({ success: false, error: 'Invalid request parameters or sock is not initialized' });
-//     }
-// });
-
-// app.post('/sendMessage', (req, res) => {
-//     const { remoteJid, message } = req.body;
-
-//     if (remoteJid && message && sock) {
-//         sock.sendMessage(remoteJid, { text: message });
-//         res.json({ success: true });
-//     } else {
-//         res.status(400).json({ success: false, error: 'Invalid request body or sock is not initialized' });
-//     }
-// });
-
-
 //----- WhatsApp API Setup -----//
 async function connectToWhatsApp() {
     const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys');
@@ -118,9 +54,6 @@ async function connectToWhatsApp() {
                 } catch (error) {
                     console.error('Error making API request:', error);
                 }
-
-            // // Additional actions if needed
-            // console.log("QR Code Data: " + qrCodeData);
             }
         });
 
@@ -138,47 +71,18 @@ async function connectToWhatsApp() {
             sock.ev.on('messages.upsert', async (messageInfoUpsert) => {
                 console.log(JSON.stringify(messageInfoUpsert, undefined, 2));
         
-                const message = messageInfoUpsert.messages[0];
-                const remoteJid = message.key.remoteJid;
-                const fromMe = message.key.fromMe;
-        
-                if (!fromMe) {
-                    console.log('Received Message:', message);
-        
-                    // Make an HTTP POST request to the API with the entire messageInfoUpsert object
-                    await sendToAPI(remoteJid, messageInfoUpsert);
-        
-                    if (message.message.audioMessage) {
-                        // Handle audioMessage
-                        const audioMessage = message.message.audioMessage;
-                        const audioData = { /* ... */ };
-                        await sendToAPI(remoteJid, audioMessage);
-                        console.log(`Audio Data: ${JSON.stringify(message.message.audioMessage)}`);
-                    } else if (message.message.imageMessage) {
-                        // Handle imageMessage
-                        const imageMessage = message.message.imageMessage;
-                        const imageData = { /* ... */ };
-                        await sendToAPI(remoteJid, imageMessage);
-                        console.log(`Image Data: ${JSON.stringify(message.message.imageMessage)}`);
-                    } else if (message.message.videoMessage) {
-                        // Handle videoMessage
-                        const videoMessage = message.message.videoMessage;
-                        const videoData = { /* ... */ };
-                        await sendToAPI(remoteJid, videoMessage);
-                        console.log(`Video Data: ${JSON.stringify(message.message.videoMessage)}`);
-                    } else if (message.message.conversation) {
-                        // Handle normal text message
-                        const textMessage = message.message.conversation;
-                        const textData = {
-                            text: textMessage
-                            // Add other properties as needed
-                        };
-        
-                        // Make an HTTP POST request to the API with textData
-                        await sendToAPI(remoteJid, textData);
-                        console.log(`Text Data: ${JSON.stringify(message.message.conversation)}`);
-                    }
-                }
+                const messageText = messageInfoUpsert;
+
+                const apiEndpoint = 'https://www.api-controller.com/api/whatsapp_qr_api.php';
+                axios.post(apiEndpoint, {
+                    //fromJid: remoteJid
+                    message: messageText
+                }).then(apiResponse => {
+                    console.log('API Response:', apiResponse.data);
+                    console.log('Data: ', messageText);
+                }).catch(error => {
+                    console.error('Error making API request:', error);
+                });
             });
         }
     });
@@ -189,30 +93,6 @@ async function connectToWhatsApp() {
 
     sock.ev.on('creds.update', saveCreds);
 }
-
-async function sendToAPI(remoteJid, messageInfoUpsert) {
-    try {
-        const apiEndpoint = 'https://www.api-controller.com/api/whatsapp_qr_api.php';
-        const apiResponse = await axios.post(apiEndpoint, {
-            remoteJid: remoteJid,
-            messageInfoUpsert: messageInfoUpsert
-        });
-
-        // Log the response from the API
-        console.log('API Response:', apiResponse.data);
-    } catch (error) {
-        console.error('Error making API request:', error);
-    }
-}
-
-                    // if (remoteJid && !fromMe) {
-                    //     console.log('Sending 10 seconds delay text messages');
-    
-                    //     setTimeout(() => {
-                    //         sock.sendMessage(remoteJid, { text: 'This message from server WhatsApp Business' });
-                    //         console.log('Delayed reply sent');
-                    //     }, 10000);
-                    // }
 
 
 //----- Execute WhatsApp API -----//
